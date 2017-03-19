2166b0b02b462ad7f060b01fc9509510dc129e19
Vansh Badkul <vanshbadkul@gmail.com>
2017-03-19 16:01:58 +0530

    Login

--- app/src/main/java/app/com/example/vansh/wdyw/activity/BorrowerLoginActivity.java
+++ app/src/main/java/app/com/example/vansh/wdyw/activity/BorrowerLoginActivity.java
@@ -2,10 +2,12 @@ package app.com.example.vansh.wdyw.activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -41,7 +43,16 @@ public class BorrowerLoginActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_login);
+
+
+
 
         ButterKnife.bind(this);
         
--- app/src/main/java/app/com/example/vansh/wdyw/activity/LLoginActivity.java
+++ app/src/main/java/app/com/example/vansh/wdyw/activity/LLoginActivity.java
@@ -2,6 +2,7 @@ package app.com.example.vansh.wdyw.activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -41,6 +42,11 @@ public class LLoginActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
